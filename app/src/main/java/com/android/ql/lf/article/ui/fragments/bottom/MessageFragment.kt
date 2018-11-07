680852a32fb3d060ca74c1d7a714fe34b4d64969
qqxliufeng <596484713@qq.com>
2018-11-07 18:05:30 +0800

    开发聊天界面

--- app/src/main/java/com/android/ql/lf/article/ui/fragments/bottom/MessageFragment.kt
+++ app/src/main/java/com/android/ql/lf/article/ui/fragments/bottom/MessageFragment.kt
@@ -1,5 +1,6 @@
 package com.android.ql.lf.article.ui.fragments.bottom
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.support.v7.widget.DividerItemDecoration
@@ -7,6 +8,8 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.TextView
 import com.android.ql.lf.article.R
+import com.android.ql.lf.article.ui.activity.ChatActivity
+import com.android.ql.lf.article.ui.fragments.community.ChatFragment
 import com.android.ql.lf.article.ui.fragments.other.ArticleWebViewFragment
 import com.android.ql.lf.baselibaray.ui.fragment.BaseRecyclerViewFragment
 import com.chad.library.adapter.base.BaseQuickAdapter
@@ -32,23 +35,24 @@ class MessageFragment : BaseRecyclerViewFragment<String>() {
 
     override fun getLayoutId() = R.layout.fragment_message_layout
 
-    override fun createAdapter() = object : BaseQuickAdapter<String, BaseViewHolder>(R.layout.adapter_message_bottom_item_layout, mArrayList) {
-        override fun convert(helper: BaseViewHolder?, item: String?) {
+    override fun createAdapter() =
+        object : BaseQuickAdapter<String, BaseViewHolder>(R.layout.adapter_message_bottom_item_layout, mArrayList) {
+            override fun convert(helper: BaseViewHolder?, item: String?) {
 
+            }
         }
-    }
 
     override fun initView(view: View?) {
         super.initView(view)
         mBaseAdapter.addHeaderView(headerView)
         mFocus.setOnClickListener {
-            ArticleWebViewFragment.startArticleWebViewFragment(mContext,"关注","attention.html")
+            ArticleWebViewFragment.startArticleWebViewFragment(mContext, "关注", "attention.html")
         }
         mCollection.setOnClickListener {
             ArticleWebViewFragment.startArticleWebViewFragment(mContext, "收藏", "collect.html")
         }
         mComment.setOnClickListener {
-            ArticleWebViewFragment.startArticleWebViewFragment(mContext,"评论","comment.html")
+            ArticleWebViewFragment.startArticleWebViewFragment(mContext, "评论", "comment.html")
         }
     }
 
@@ -64,5 +68,8 @@ class MessageFragment : BaseRecyclerViewFragment<String>() {
         return itemDecoration
     }
 
-
+    override fun onMyItemClick(adapter: BaseQuickAdapter<*, *>?, view: View?, position: Int) {
+        super.onMyItemClick(adapter, view, position)
+        startActivity(Intent(mContext,ChatActivity::class.java))
+    }
 }
--- app/src/main/java/com/android/ql/lf/article/utils/ContextHelper.kt
+++ app/src/main/java/com/android/ql/lf/article/utils/ContextHelper.kt
@@ -1,8 +1,11 @@
 package com.android.ql.lf.article.utils
 
+import android.app.Activity
 import android.content.Context
 import android.support.v4.app.Fragment
 import android.util.DisplayMetrics
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import org.jetbrains.anko.windowManager
 
 fun Context.getScreen(): Pair<Int, Int> {
@@ -12,3 +15,14 @@ fun Context.getScreen(): Pair<Int, Int> {
 }
 
 fun Fragment.getScreen() = context?.getScreen()
+
+
+fun Context.showSoftInput(view:View) {
+    val imm = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.showSoftInput(view, InputMethodManager.SHOW_FORCED)
+}
+
+fun Context.hideSoftInput(view:View) {
+    val imm = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
+}
